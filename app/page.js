ef46fe82884c954d7307ceaaa32cd62cc7372f87
SuperMeteo <sakayking01@gmail.com>
2024-07-04 13:28:43 +0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -14,19 +14,30 @@ export default function Home() {
       </div>
       </div>
       <br></br><br></br>  
-      <div class="container">
-        <div class="row">
-            <div class="col">
-                <img src="/img/q1.jpg" class="img-fluid" alt="Image 1"></img>
-            </div>
-            <div class="col">
-                <img src="/img/q2.jpg" class="img-fluid" alt="Image 2"></img>
-            </div>
-            <div class="col">
-                <img src="/img/q3.jpg" class="img-fluid" alt="Image 3"></img>
-            </div>
-        </div>
+
+    <div class="card-group">
+  <div class="card">
+    <img src="/img/q1.jpg" class="card-img-top" alt="..."></img>
+    <div class="card-body">
+      <h5 class="card-title">Ai ปัญญาประดิษฐ์</h5>
+      <p class="card-text">โปรแกรมคอมพิวเตอร์ที่มีฟังก์ชันที่สามารถทำงานได้เหมือนกับมนุษย์ และสามารถเลียนแบบการทำกิจกรรมของมนุษย์ได้ เช่น การเรียนรู้ การวางแผน และการแก้ไขปัญหาต่างๆ เป็นตัวช่วยมนุษย์ในการคิด ซึ่งจะเน้นไปในเรื่องของการประมวลผล และวิเคราะห์ข้อมูลต่างๆ</p>
     </div>
+  </div>
+  <div class="card">
+    <img src="/img/q2.jpg" class="card-img-top" alt="..."></img>
+    <div class="card-body">
+      <h5 class="card-title">AI จะครองโลกแทนมนุษย์…แน่หรือ</h5>
+      <p class="card-text">AI จะครองโลกแทนมนุษย์ไหม ปัญญาประดิษฐ์จะทำอะไรกับมนุษย์บ้าง ผลกระทบ และความกังวลมีอะไรบ้าง ขอเชิญทุกท่านนั่งลงอย่างใจเย็นรวบรวมสมาธิ แล้วเพลิดเพลินกับบทความที่ให้สาระและการเคลื่อนไหวในโลก Big Data ไปกับเรา</p>
+    </div>
+  </div>
+  <div class="card">
+    <img src="/img/q3.jpg" class="card-img-top" alt="..."></img>
+    <div class="card-body">
+      <h5 class="card-title">5 ประโยชน์ของระบบ AI กับการประยุกต์ใช้ในธุรกิจ</h5>
+      <p class="card-text">1. AI ช่วยให้การทำ Personalized Marketing แม่นยำมากขึ้น · 2. สำรวจพฤติกรรมลูกค้าผ่านระบบ Facial Recognition · 3. Robotics อนาคตแห่งการขนส่งสินค้า · 4. AI ตัวช่วยจัดการปัญหาคลังสินค้า.</p>
+    </div>
+  </div>
+</div>
     <br></br><br></br><br></br><br></br>
     <Footer />
    </>
