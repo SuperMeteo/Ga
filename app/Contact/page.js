20a0bdd35f142a507f1bff65072c853688955ac2
pu <sakayking01@gmail.com>
2024-07-04 13:19:51 +0700

    last

--- app/Contact/page.js
+++ app/Contact/page.js
@@ -1,37 +1,35 @@
 import Navbar from '/app/component/nav';
 import Footer from '/app/component/footer';
+
 export default function About() {
   return (
     <>
       <Navbar />
-      <div className="row">
-        <div
-          className="col-lg-6 mx-auto about"
-          style={{
-            backgroundColor: '#ffffff', // พื้นหลังสีขาว
-            padding: '20px',
-            borderRadius: '10px',
-            boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)',
-          }}
-        >
-          <h2 className="text-center mb-4" style={{ color: '#333' }}>
-            Contact
-          </h2>
-          
-       
-          <p style={{ color: '#666' }}>
-          Email : ai@Ai.com
-          <br></br>
-          Email : Ai2@ai.com
-          </p>
-          <p style={{ color: '#666' }}>
-           เบอร์โทร : 841111158
-          </p>
-          
+      <div className="container mt-5">
+        <div className="row">
+          <div className="col-lg-6 mx-auto">
+            <div
+              className="about"
+              style={{
+                backgroundColor: '#ffffff',
+                padding: '20px',
+                borderRadius: '10px',
+                boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)',
+              }}
+            >
+              <h2 className="text-center mb-4" style={{ color: '#333' }}>
+                Contact
+              </h2>
+              <p style={{ color: '#666' }}>
+                Email: ai@Ai.com <br />
+                Email: Ai2@ai.com
+              </p>
+              <p style={{ color: '#666' }}>เบอร์โทร: 841111158</p>
+            </div>
+          </div>
         </div>
       </div>
-      <br />
-      <br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br> <br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
+      <br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
       <Footer />
     </>
   );
